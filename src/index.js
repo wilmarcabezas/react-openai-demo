e61472c1051863302ec43d4eb60163c8f2e98663
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-01-13 13:27:46 -0500

    Agregar componente ConnectAPI

--- src/index.js
+++ src/index.js
@@ -1,33 +1,39 @@
 import ReactDOM from "react-dom";
-import ConnectAPI from "./components/ConnectAPI";
 import { useState } from "react";
+import ConnectAPI from "./components/ConnectAPI";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
         <h1>Welcome to ReactOPENAI</h1>
         <Formulario/>
+        <ConnectAPI />
         <hr />
         
     </>
 )
 
 function Formulario() {
-    const [message, setMessage]=  useState('')
+    const [messagePrompt, setMessagePrompt]=  useState('')
 
     const handleInput = (event)=>{
-        console.log(event.target.value)
-        setMessage(event.target.value)
+        setMessagePrompt(event.target.value)
     }
 
     const handleSubmit = (event)=>{
         event.preventDefault();
-        console.log(message)
+        console.log(messagePrompt)
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <textarea onChange={handleInput} name="prompt" placeholder="Write a prompt" /><br />
+            <textarea 
+            onChange={handleInput} 
+            name="prompt" 
+            placeholder="Write a prompt" />
+            
+            <br />
+
             <button type="submit">Enviar</button>
         </form>
     )
