871c401b602b97443c93deedaa57ba07c2838c18
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2024-01-13 12:53:10 -0600

    Se creo el useState prompFinalizado

--- src/index.js
+++ src/index.js
@@ -4,44 +4,42 @@ import ConnectAPI from "./components/ConnectAPI";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <>
-        <h1>Welcome to ReactOPENAI</h1>
-        <Formulario/>
-        <hr />
-        
-    </>
-)
+  <>
+    <h1>Welcome to ReactOPENAI</h1>
+    <Formulario />
+    <hr />
+  </>
+);
 
 function Formulario() {
-    const [messagePrompt, setMessagePrompt]=  useState('')
+  const [messagePrompt, setMessagePrompt] = useState("");
+  const [promptFinalizado, setPromptFinalizado] = useState(false);
 
-    const handleInput = (event)=>{
-        setMessagePrompt(event.target.value)
-    }
+  const handleInput = (event) => {
+    setMessagePrompt(event.target.value);
+  };
 
-    const handleSubmit = (event)=>{
-        event.preventDefault();
-        console.log(messagePrompt)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(messagePrompt);
+    setPromptFinalizado(true);
+  };
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <textarea 
-                onChange={handleInput} 
-                name="prompt" 
-                placeholder="Write a prompt" />
-                
-                <br />
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          onChange={handleInput}
+          name="prompt"
+          placeholder="Write a prompt"
+        />
 
-                <button type="submit">Enviar</button>
+        <br />
 
-            </form>
-            
-            {messagePrompt&&
-                <ConnectAPI message= {messagePrompt} />
-            }
-            
-        </>
-    )
+        <button type="submit">Enviar</button>
+      </form>
+
+      {promptFinalizado && <ConnectAPI message={messagePrompt} />}
+    </>
+  );
 }
